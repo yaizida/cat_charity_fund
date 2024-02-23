1352bdd1bc1a336d32f8c3188fe6627af62f98b7
Никита Бурковский <myizizda@gmail.com>
2024-02-23 17:55:36 +0300

    create stock value for database_url

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ from pydantic import BaseSettings, EmailStr
 class Settings(BaseSettings):
     app_tittle: str = 'Фонд помощи животным'
     app_description: str = 'Этот серивис помогает животным'
-    database_url: str
+    database_url: str = 'sqlite+aiosqlite:///./fastapi.db'
     secret: str = 'SECRET'
     first_superuser_email: Optional[EmailStr] = None
     first_superuser_password: Optional[str] = None
