cc47264e860f371383295237df252715a3946124
Никита Бурковский <myizizda@gmail.com>
2024-02-26 01:54:11 +0300

    xxxx

--- app/models/abstract.py
+++ app/models/abstract.py
@@ -10,13 +10,14 @@ class Abstract(Base):
 
     __abstract__ = True
 
+    __table_args__ = (
+        CheckConstraint('full_amount >= 0', name='check_full_amount_positive'),
+        CheckConstraint('invested_amount >= 0', name='check_invested_amount_positive'),
+        CheckConstraint('invested_amount <= full_amount', name='check_invested_amount_not_exceed_full')
+    )
+
     full_amount = Column(Integer, nullable=False)
     invested_amount = Column(Integer, default=DEFAULT_INVESTED_AMOUNT)
     fully_invested = Column(Boolean, default=False)
     create_date = Column(DateTime, default=datetime.now)
     close_date = Column(DateTime)
-
-    __table_args__ = (
-        CheckConstraint('invested_amount > 0', name='check_invested_amount_positive'),
-        CheckConstraint('invested_amount <= full_amount', name='check_invested_amount_not_exceed_full')
-    )
